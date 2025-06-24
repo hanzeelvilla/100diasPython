b974756711c84a3103afab10497259cd2a83543b
Hanzeel Villa <hanzeelvilla0@gmail.com>
2025-06-24 12:30:26 -0600

    proyecto

---
+++ dia1/proyecto/main.py
@@ -0,0 +1,4 @@
+print("Bienvenido al generador de nombres de bandas 🤘")
+city = input("En que ciudad creciste? ")
+nombre_mascota = input("Como se llama tu mascota? ")
+print("Tu nombre de banda es: " + city + nombre_mascota)
