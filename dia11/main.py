cd514169b39041e9df6433df14c18440373526d8
Hanzeel Villa <hanzeelvilla0@gmail.com>
2025-07-14 11:41:56 -0600

    Loop juego

--- dia11/main.py
+++ dia11/main.py
@@ -1,17 +1,40 @@
 import random
 
-CARDS = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-player_cards = []
-pc_cards = []
+CARTAS = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+cartas_jugador = []
+cartas_pc = []
 
 for i in range(2):
-    player_cards.append(random.choice(CARDS))
-    pc_cards.append(random.choice(CARDS))
+    cartas_jugador.append(random.choice(CARTAS))
+    cartas_pc.append(random.choice(CARTAS))
 
-print(f"PC CARDS: {pc_cards}")
-print(f"PLAYER CARDS: {player_cards}")
+score_jugador = sum(cartas_jugador)
+score_pc = sum(cartas_pc)
+nueva_carta = True
 
-if sum(player_cards) == 21:
+print(f"Cartas de la PC: [{cartas_pc[0]}, ?]")
+print(f"Tus cartas: {cartas_jugador}")
+print(f"Tu score: {score_jugador}")
+
+while score_jugador < 21 and score_pc < 21 and nueva_carta:
+    opcion = input("Quieres otra carta? 'si' o 'no': ").lower()
+
+    if opcion == "no":
+        nueva_carta = False
+    else:
+        cartas_jugador.append(random.choice(CARTAS))
+        score_jugador = sum(cartas_jugador)
+    
+    print(f"Tus cartas: {cartas_jugador}")
+    print(f"Tu score: {score_jugador}")
+
+# Dar resultados del juego
+if score_jugador == 21:
     print("Has ganado")
-elif sum(pc_cards) == 21:
+    print(f"Tus cartas: {cartas_jugador}")
+elif score_pc == 21:
     print("La computadora gana")
+    print(f"Cartas de la PC: {cartas_pc}")
+elif score_jugador > 21:
+    print("Te has pasado")
+    #print(f"Tus cartas: {cartas_jugador}")
