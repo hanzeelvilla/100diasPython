4d22ccc3dad0794ee354232f26d91c5ca11827fe
Hanzeel Villa <hanzeelvilla0@gmail.com>
2025-07-18 08:48:05 -0600

    Juego acabado

--- dia11/main.py
+++ dia11/main.py
@@ -36,19 +36,35 @@ while score_jugador < 21 and score_pc < 21 and nueva_carta:
         cartas_jugador.append(random.choice(CARTAS))
         score_jugador = calcular_score(cartas_jugador)
     
-    print(f"Tus cartas: {cartas_jugador}")
-    print(f"Tu score: {score_jugador}")
+    print(f"Tus cartas: {cartas_jugador} = {score_jugador}")
     print("--------------------------")
-
-
 
 # Dar resultados del juego
 if score_jugador == 21:
     print("Has ganado")
-    print(f"Tus cartas: {cartas_jugador}")
-elif score_pc == 21:
-    print("La computadora gana")
-    print(f"Cartas de la PC: {cartas_pc}")
+    print(f"Tus cartas: {cartas_jugador} = {score_jugador}")
 elif score_jugador > 21:
     print("Te has pasado")
-    #print(f"Tus cartas: {cartas_jugador}")
+    print(f"Tus cartas: {cartas_jugador} = {score_jugador}")
+else:
+    print("Turno de la computadora")
+
+    while score_pc < 17 and score_pc < 21:
+        cartas_pc.append(random.choice(CARTAS))
+        score_pc = calcular_score(cartas_pc)
+
+    # Dar resultados del juego
+    if score_pc == 21:
+        print("La computadora gana")
+    elif score_pc > 21:
+        print("La computadora se ha pasado")
+        print("Has ganado")
+    elif score_pc > score_jugador:
+        print("La computadora gana")
+    elif score_jugador > score_pc:
+        print("Has ganado")
+    else:
+        print("Empate")
+
+    print(f"Cartas de la PC: {cartas_pc} = {score_pc}")
+    print(f"Tus cartas: {cartas_jugador} = {score_jugador}")
