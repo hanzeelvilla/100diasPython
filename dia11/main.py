0394440d7f0103fecf0e88cb64f78311cf3f3d7c
Hanzeel Villa <hanzeelvilla0@gmail.com>
2025-07-18 08:32:54 -0600

    Repartir cartas fix

--- dia11/main.py
+++ dia11/main.py
@@ -4,17 +4,28 @@ CARTAS = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 cartas_jugador = []
 cartas_pc = []
 
+def calcular_score(cartas):
+    score = sum(cartas)
+
+    if 11 in cartas and score > 21:
+        cartas.remove(11)
+        cartas.append(1)
+        score = sum(cartas)
+    
+    return score
+
 for i in range(2):
     cartas_jugador.append(random.choice(CARTAS))
     cartas_pc.append(random.choice(CARTAS))
 
-score_jugador = sum(cartas_jugador)
-score_pc = sum(cartas_pc)
+score_jugador = calcular_score(cartas_jugador)
+score_pc = calcular_score(cartas_pc)
 nueva_carta = True
 
 print(f"Cartas de la PC: [{cartas_pc[0]}, ?]")
 print(f"Tus cartas: {cartas_jugador}")
 print(f"Tu score: {score_jugador}")
+print("--------------------------")
 
 while score_jugador < 21 and score_pc < 21 and nueva_carta:
     opcion = input("Quieres otra carta? 'si' o 'no': ").lower()
@@ -23,10 +34,13 @@ while score_jugador < 21 and score_pc < 21 and nueva_carta:
         nueva_carta = False
     else:
         cartas_jugador.append(random.choice(CARTAS))
-        score_jugador = sum(cartas_jugador)
+        score_jugador = calcular_score(cartas_jugador)
     
     print(f"Tus cartas: {cartas_jugador}")
     print(f"Tu score: {score_jugador}")
+    print("--------------------------")
+
+
 
 # Dar resultados del juego
 if score_jugador == 21:
