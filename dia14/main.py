60e80ea4d33787c554b38e7e3b3db04f522e9627
Hanzeel Villa <hanzeelvilla0@gmail.com>
2025-07-21 11:20:46 -0600

    Juego terminado

--- dia14/main.py
+++ dia14/main.py
@@ -1,31 +1,47 @@
 import random
 from game_data import data
 
-def comparar_seguidores(persona1, persona2):
-    if(persona1["seguidores"] > persona2["seguidores"]):
+def comparar_seguidores(num1, num2):
+    if(num1 > num2):
         return "A"
-    elif(persona2["seguidores"] > persona1["seguidores"]):
+    elif(num2 > num1):
         return "B"
     else:
-        "igual"
+        return "igual"
+
+def formatear_seguidores(num_seguidores):
+    return "{:,}".format(num_seguidores)
+
+jugar = True
+score = 0
 
 persona1 = random.choice(data)
 persona2 = random.choice(data)
 
 print("Bienvenido a Higher or Lower edición chafa\n")
-print("Quién tiene más seguidores?")
-print(f"A: {persona1["nombre"]} {persona1["descripcion"]} De {persona1["pais"]}")
-print(f"B: {persona2["nombre"]} {persona2["descripcion"]} De {persona2["pais"]}")
 
-res = input("Elije 'A' o 'B': ").upper()
-persona_con_mas_seguidodres = comparar_seguidores(persona1, persona2)
+while jugar:
+    print("Quién tiene más seguidores?")
+    print(f"A: {persona1["nombre"]} {persona1["descripcion"]} De {persona1["pais"]}")
+    print(f"B: {persona2["nombre"]} {persona2["descripcion"]} De {persona2["pais"]}")
 
-if res == persona_con_mas_seguidodres :
-    print("Has adivinado")
-elif persona_con_mas_seguidodres == "igual":
-    print("Tienen los mismos")
-else:
-    print("Has perdido")
+    res = input("Elije 'A' o 'B': ").upper()
+    persona_con_mas_seguidodres = comparar_seguidores(persona1["seguidores"], persona2["seguidores"])
 
-print(f"{persona1["nombre"]}: {persona1["seguidores"]} seguidores")
-print(f"{persona2["nombre"]}: {persona2["seguidores"]} seguidores")
+    print(f"{persona1["nombre"]}: {formatear_seguidores(persona1["seguidores"])} seguidores")
+    print(f"{persona2["nombre"]}: {formatear_seguidores(persona2["seguidores"])} seguidores")
+
+    if res == persona_con_mas_seguidodres or persona_con_mas_seguidodres == "igual":
+        persona1 = persona2
+        persona2 = random.choice(data)
+        print("Correcto")
+        score += 1
+
+        print(f"Score: {score}")
+    else:
+        print("Has perdido")
+        jugar = False
+
+    print("-----------------------------------------------------------------------------------------------------------------------")
+
+print(f"Score: {score}")
