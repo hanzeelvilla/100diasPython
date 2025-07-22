11bc0127c2039c4ca1473de94d2e02caca66559a
Hanzeel Villa <hanzeelvilla0@gmail.com>
2025-07-22 10:44:04 -0600

    Verificar recursos

--- dia15/main.py
+++ dia15/main.py
@@ -1,6 +1,18 @@
-recursos = {
-    "agua": 100,
-    "leche": 50,
+from menu import MENU
+
+def hay_recursos(tipo_cafe):
+    ingredientes = tipo_cafe["ingredientes"]
+    
+    for key in ingredientes:
+        if RECURSOS[key] < ingredientes[key]:
+            return False
+    
+    return True
+
+RECURSOS = {
+    "agua": 300,
+    "leche": 200,
+    "cafe": 100,
     "dinero": 0
 }
 
@@ -10,10 +22,17 @@ while maquina_encendida:
     res = input("Qué quieres ordenar? (expresso/latte/capuccino): ").lower()
 
     if res == "reporte":
-        for key in recursos:
-            print(f"{key}: {recursos[key]}")
-    elif(res == "off"):
+        for key in RECURSOS:
+            print(f"{key}: {RECURSOS[key]}")
+    elif res == "off":
         print("Apagando máquina")
         maquina_encendida = False
+    elif res == "expresso" or res == "latte" or res == "capuccino":
+        cafe = MENU[res]
+        hay_recursos(cafe)
+        if hay_recursos(cafe):
+            print("Hay suficientes recursos")
+        else:
+            print(f"No hay suficientes recursos para hacer un {res}")
     else:
         print("Opción no válida")
---
+++ dia15/menu.py
@@ -0,0 +1,25 @@
+MENU = {
+    "expresso": {
+        "ingredientes": {
+            "agua": 50,
+            "cafe": 18,
+        },
+        "precio": 1.5
+    },
+    "latte": {
+        "ingredientes": {
+            "agua": 200,
+            "leche": 150,
+            "cafe": 24
+        },
+        "precio": 2.5
+    },
+    "capuccino": {
+        "ingredientes": {
+            "agua": 250,
+            "leche": 100,
+            "cafe": 24
+        },
+        "precio": 3
+    }
+}
