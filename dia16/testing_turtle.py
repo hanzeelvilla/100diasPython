e97599224f666073b77c70b1c745023055bb8a70
Hanzeel Villa <hanzeelvilla0@gmail.com>
2025-07-25 09:50:57 -0600

    Testing turtle

---
+++ dia16/testing_turtle.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+turtle = Turtle()
+screen = Screen()
+turtle.shape("turtle")
+turtle.color("red")
+turtle.forward(100)
+
+print(turtle)
+print(screen.canvheight)
+screen.exitonclick()
